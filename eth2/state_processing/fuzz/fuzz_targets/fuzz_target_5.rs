696c90fc1581dccb1ed9195e564b56b4c1412592
Mehdi Zerouali <mehdi@sigmaprime.io>
2020-05-04 08:33:13 +1000

    Add fuzzing target for process_final_updates (epoch processing)

---
+++ eth2/state_processing/fuzz/fuzz_targets/fuzz_target_5.rs
@@ -0,0 +1,17 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use state_processing::per_epoch_processing::process_final_updates;
+use types::*;
+
+type TestEthSpec = MinimalEthSpec;
+
+fuzz_target!(|wrapper: (BeaconState<TestEthSpec>)| {
+    // Get default spec
+    let spec = TestEthSpec::default_spec();
+
+    // Upnack arbitrary values
+    let mut state = wrapper;
+
+    // Fuzz Target
+    process_final_updates(&mut state, &spec);
+});
